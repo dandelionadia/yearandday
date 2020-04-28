38b6be5ef3f8de6c3a3e796368bb96d419a2b6f5
dandelionadia <nadia.ridko@gmail.com>
2020-04-28 17:39:01 +0200

    atoms: adds card link and image componets

---
+++ src/atoms/CardLink.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CardLink = styled.a`
+  background-color: ${({ theme }) => theme.colors.oringLight};
+  text-align: center;
+  padding: 1rem 0.8rem;
+`;
